1181620409ddb187fdffdee57aec683bf271eb24
lionryin <710523800@qq.com>
2015-06-02 19:15:04 +0800

    Initial Commit

---
+++ LXSegmentViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LXSegmentViewDemo
+//
+//  Created by MacAir2 on 15/6/2.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
